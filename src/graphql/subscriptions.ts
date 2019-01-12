fa00b40f000cacd1033ee0c844fc924487d764f5
Alex Maingot <alex.maingot@gmail.com>
2019-01-12 15:19:33 -0600

    Starting my amplify project

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,30 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    name
+    description
+    finished
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    name
+    description
+    finished
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    name
+    description
+    finished
+  }
+}
+`;
