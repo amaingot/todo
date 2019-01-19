8682f59baa3e131db9672a22028dcce854046f9a
Alex Maingot <alex.maingot@gmail.com>
2019-01-19 17:21:17 -0600

    Pushing to AWS

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,33 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const createTodo = `mutation CreateTodo($input: CreateTodoInput!) {
+  createTodo(input: $input) {
+    id
+    name
+    description
+    owner
+    group
+  }
+}
+`;
+export const updateTodo = `mutation UpdateTodo($input: UpdateTodoInput!) {
+  updateTodo(input: $input) {
+    id
+    name
+    description
+    owner
+    group
+  }
+}
+`;
+export const deleteTodo = `mutation DeleteTodo($input: DeleteTodoInput!) {
+  deleteTodo(input: $input) {
+    id
+    name
+    description
+    owner
+    group
+  }
+}
+`;
