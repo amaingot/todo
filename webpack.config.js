0f179d3a45cb83ec0addec1daaf51da592592c8a
Alex Maingot <alex.maingot@gmail.com>
2019-01-26 23:34:24 -0600

    Fix build for circleci

--- webpack.config.js
+++ webpack.config.js
@@ -7,48 +7,6 @@ const S3_BUCKET_URL = "https://todo.maingot.us/";
 const CIRCLE_BRANCH = process.env.CIRCLE_BRANCH || "production";
 const ASSET_PATH = `${S3_BUCKET_URL}${CIRCLE_BRANCH}/`;
 
-const devConfig = {
-  mode: "development",
-  devtool: "source-map",
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    host: "localhost",
-    disableHostCheck: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    https: {
-      key: fs.readFileSync("certs/localhost.key"),
-      cert: fs.readFileSync("certs/localhost.crt")
-    },
-    port: 9000
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
-    publicPath: "https://localhost:9000/"
-  }
-};
-
-const prodConfig = {
-  mode: "production",
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.DefinePlugin({
-      "process.env.CIRCLE_BRANCH": JSON.stringify(CIRCLE_BRANCH),
-      "process.env.S3_BUCKET_URL": JSON.stringify(S3_BUCKET_URL)
-    })
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
-    publicPath: ASSET_PATH
-  }
-};
-
 module.exports = env => {
   const baseConfig = {
     entry: "./src/index.tsx",
@@ -71,14 +29,47 @@ module.exports = env => {
   if (env.prod) {
     return {
       ...baseConfig,
-      ...prodConfig
-    };
+      mode: "production",
+      plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.DefinePlugin({
+          "process.env.CIRCLE_BRANCH": JSON.stringify(CIRCLE_BRANCH),
+          "process.env.S3_BUCKET_URL": JSON.stringify(S3_BUCKET_URL)
+        })
+      ],
+      output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build"),
+        publicPath: ASSET_PATH
+      }    };
   }
 
   if (env.dev) {
     return {
       ...baseConfig,
-      ...devConfig
+      mode: "development",
+      devtool: "source-map",
+      devServer: {
+        historyApiFallback: true,
+        hot: true,
+        host: "localhost",
+        disableHostCheck: true,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        https: {
+          key: fs.readFileSync("certs/localhost.key"),
+          cert: fs.readFileSync("certs/localhost.crt")
+        },
+        port: 9000
+      },
+      plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+      ],
+      output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build"),
+        publicPath: "https://localhost:9000/"
+      }
     };
   }
 };
