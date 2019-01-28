3ce3af37abea113e787f6ab072df51bfda529564
Alex Maingot <alex.maingot@gmail.com>
2019-01-28 17:36:57 -0600

    Adding withTodoList enhancer

---
+++ src/enhancers/withTodoList.tsx
@@ -0,0 +1,43 @@
+import { DataValue, graphql, QueryOpts } from 'react-apollo';
+
+import { buildSubscription } from 'aws-appsync';
+import { listTodos } from '../graphql/queries';
+import { onCreateTodo, onDeleteTodo, onUpdateTodo } from '../graphql/subscriptions';
+import { ListTodosQueryData, ListTodosQueryVariables } from '../graphql/types';
+
+export interface WithTodoListProps extends DataValue<ListTodosQueryData, ListTodosQueryVariables> {
+  subscribeToUpdateTodo: () => void;
+  subscribeToCreateTodo: () => void;
+  subscribeToDeleteTodo: () => void;
+}
+
+const withTodoList = <TProps extends {}>(
+  variables?: (props: TProps) => QueryOpts<ListTodosQueryVariables>
+) =>
+  graphql<TProps, ListTodosQueryData, ListTodosQueryVariables, WithTodoListProps>(listTodos, {
+    options: props => {
+      const generatedOptions = variables ? variables(props) : {};
+      return {
+        fetchPolicy: 'cache-and-network',
+        ...generatedOptions,
+      };
+    },
+    props: r => {
+      const { data } = r;
+      if (!data) {
+        // We need to figure out something cool to do here.
+        throw new Error();
+      }
+      return {
+        subscribeToUpdateTodo: () =>
+          data.subscribeToMore(buildSubscription(onUpdateTodo, listTodos)),
+        subscribeToCreateTodo: () =>
+          data.subscribeToMore(buildSubscription(onCreateTodo, listTodos)),
+        subscribeToDeleteTodo: () =>
+          data.subscribeToMore(buildSubscription(onDeleteTodo, listTodos)),
+        ...data,
+      };
+    },
+  });
+
+export default withTodoList;
