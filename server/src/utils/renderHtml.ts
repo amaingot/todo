8fbf2dac0cd53ad732e4e18d4853d6b217e794e4
Alex Maingot <alex.maingot@gmail.com>
2020-05-24 00:08:42 -0500

    Fix html rendering

--- server/src/utils/renderHtml.ts
+++ server/src/utils/renderHtml.ts
@@ -11,20 +11,18 @@ const renderHtml = (app: Application) => {
       ? path.join(__dirname, "../../assets")
       : path.join(__dirname, "../../../web/build");
 
-  const rawHtml = fs.readFileSync(`${assetPath}/index.html`, "utf8");
-
-  const TENANT_ID = config.get("GCP_IDP_TENANT_ID");
-  const FIREBASE_CONFIG = config.get("FIREBASE_APP_CONFIG");
-
-  const html = Mustache.render(rawHtml, {
-    TENANT_ID,
-    FIREBASE_CONFIG,
-  });
-
-  app.use(express.static(assetPath));
+  app.use(express.static(assetPath, { index: false }));
 
   app.get("/*", (_req, res) => {
-    res.send(html);
+    const rawHtml = fs.readFileSync(`${assetPath}/index.html`, "utf8");
+
+    const html = Mustache.render(rawHtml, {
+      TENANT_ID: config.get("GCP_IDP_TENANT_ID"),
+      FIREBASE_CONFIG: config.get("FIREBASE_APP_CONFIG"),
+    });
+    console.log({ html });
+
+    res.status(200).send(html);
   });
 };
 
